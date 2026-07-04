466a9dcf822cefdea27efa9ca75e49b57240694c
NotPKot <notpkot@gmail.com>
2026-07-04 18:19:06 -0400

    0.1.16.2 GOAP REFERENCE KILL

--- REFERENCIA/GOAP PARA HEALER/goap-master/src/goals/gather_wood.gd
+++
@@ -1,9 +0,0 @@
-extends "res://src/goal.gd"
-
-
-func _init() -> void:
-	desired_state = { has_stock_wood = true }
-
-
-func is_valid(state: Dictionary) -> bool:
-	return 'has_stock_wood' not in state
--- REFERENCIA/GOAP PARA HEALER/goap-master/src/goals/idle.gd
+++
@@ -1,5 +0,0 @@
-extends "res://src/goal.gd"
-
-
-func get_priority(state: Dictionary) -> int:
-	return -1
